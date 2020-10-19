jimmykarns/Veloren

Initial bipedlarge beast walk animation

voxygen/src/anim/src/biped_large/idle.rs
@@ -52,29 +52,37 @@ impl Animation for IdleAnimation {
                 * 0.125,
         );
 
+        // Controls for the beast breathing
+        let intensity = 0.04;
+        let lenght = 1.0;
+        let chop = 0.2;
+        let chop_freq = 40.0;
+
+        let breathing = intensity * (lenght * anim_time as f32).sin() + 0.05 * chop * (anim_time as f32 * chop_freq).sin() * (anim_time as f32 * lenght).cos();
+
         next.head.offset = Vec3::new(
             0.0,
             skeleton_attr.head.0,
             skeleton_attr.head.1 + torso * 0.2,
         ) * 1.02;
-        next.head.ori = Quaternion::rotation_z(look.x * 0.6) * Quaternion::rotation_x(look.y * 0.6);
-        next.head.scale = Vec3::one() * 1.02;
+        next.head.ori = Quaternion::rotation_z(look.x * 0.6) * Quaternion::rotation_x(look.y * 0.6 + breathing);
+        next.head.scale = Vec3::one() * 1.02 + breathing * 0.4;
 
         next.upper_torso.offset = Vec3::new(
             0.0,
             skeleton_attr.upper_torso.0,
             skeleton_attr.upper_torso.1 + torso * 0.5,
         );
-        next.upper_torso.ori = Quaternion::rotation_z(0.0);
-        next.upper_torso.scale = Vec3::one();
+        next.upper_torso.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(- breathing);
+        next.upper_torso.scale = Vec3::one() - breathing * 0.4;
 
         next.lower_torso.offset = Vec3::new(
             0.0,
             skeleton_attr.lower_torso.0,
             skeleton_attr.lower_torso.1 + torso * 0.15,
         );
-        next.lower_torso.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
-        next.lower_torso.scale = Vec3::one() * 1.02;
+        next.lower_torso.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(breathing);
+        next.lower_torso.scale = Vec3::one() * 1.02 + breathing * 0.4;
         
         next.jaw.offset = Vec3::new(
             0.0,
@@ -108,16 +116,16 @@ impl Animation for IdleAnimation {
             skeleton_attr.shoulder.1,
             skeleton_attr.shoulder.2,
         );
-        next.shoulder_l.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
-        next.shoulder_l.scale = Vec3::one();
+        next.shoulder_l.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(breathing);
+        next.shoulder_l.scale = Vec3::one() + breathing;
 
         next.shoulder_r.offset = Vec3::new(
             skeleton_attr.shoulder.0,
             skeleton_attr.shoulder.1,
             skeleton_attr.shoulder.2,
         );
-        next.shoulder_r.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
-        next.shoulder_r.scale = Vec3::one();
+        next.shoulder_r.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(breathing);
+        next.shoulder_r.scale = Vec3::one() + breathing;
 
         next.hand_l.offset = Vec3::new(
             -skeleton_attr.hand.0,

voxygen/src/anim/src/biped_large/wield.rs
@@ -25,6 +25,22 @@ impl Animation for WieldAnimation {
         let breathe = (anim_time as f32 + 1.5 * PI).sin();
         let test = (anim_time as f32 + 36.0 * PI).sin();
 
+        let slower = (anim_time as f32 * 1.0 + PI).sin();
+        let slow = (anim_time as f32 * 3.5 + PI).sin();
+        
+        let tailmove = Vec2::new(
+            ((global_time + anim_time) as f32 / 2.0)
+                .floor()
+                .mul(7331.0)
+                .sin()
+                * 0.25,
+            ((global_time + anim_time) as f32 / 2.0)
+                .floor()
+                .mul(1337.0)
+                .sin()
+                * 0.125,
+        );
+
         let look = Vec2::new(
             ((global_time + anim_time) as f32 / 8.0)
                 .floor()
@@ -122,13 +138,22 @@ impl Animation for WieldAnimation {
             next.lower_torso.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
             next.lower_torso.scale = Vec3::one() * 1.02;
 
-            next.jaw.offset = Vec3::new(0.0, skeleton_attr.jaw.0, skeleton_attr.jaw.1 * 0.0);
-            next.jaw.ori = Quaternion::rotation_z(0.0);
-            next.jaw.scale = Vec3::one();
-
+            next.jaw.offset = Vec3::new(
+                0.0,
+                skeleton_attr.jaw.0 - slower * 0.12,
+                skeleton_attr.jaw.1 + slow * 0.2,
+            );
+            next.jaw.ori = Quaternion::rotation_x(slow * 0.05);
+            next.jaw.scale = Vec3::one() * 1.02;
+    
             next.tail.offset = Vec3::new(0.0, skeleton_attr.tail.0, skeleton_attr.tail.1);
-            next.tail.ori = Quaternion::rotation_z(0.0);
+            next.tail.ori =
+                Quaternion::rotation_z(0.0 + slow * 0.2 + tailmove.x) * Quaternion::rotation_x(0.0);
             next.tail.scale = Vec3::one();
+    
+            next.control.offset = Vec3::new(0.0, 0.0, 0.0);
+            next.control.ori = Quaternion::rotation_z(0.0);
+            next.control.scale = Vec3::one();
 
             next.shoulder_l.offset = Vec3::new(
                 -skeleton_attr.shoulder.0,
@@ -208,6 +233,23 @@ impl Animation for WieldAnimation {
             next.lower_torso.ori =
                 Quaternion::rotation_z(short * 0.15) * Quaternion::rotation_x(0.14);
             next.lower_torso.scale = Vec3::one() * 1.02;
+
+            next.jaw.offset = Vec3::new(
+                0.0,
+                skeleton_attr.jaw.0 - slower * 0.12,
+                skeleton_attr.jaw.1 + slow * 0.2,
+            );
+            next.jaw.ori = Quaternion::rotation_x(slow * 0.05);
+            next.jaw.scale = Vec3::one() * 1.02;
+    
+            next.tail.offset = Vec3::new(0.0, skeleton_attr.tail.0, skeleton_attr.tail.1);
+            next.tail.ori =
+                Quaternion::rotation_z(0.0 + slow * 0.2 + tailmove.x) * Quaternion::rotation_x(0.0);
+            next.tail.scale = Vec3::one();
+    
+            next.control.offset = Vec3::new(0.0, 0.0, 0.0);
+            next.control.ori = Quaternion::rotation_z(0.0);
+            next.control.scale = Vec3::one();
 
             next.shoulder_l.offset = Vec3::new(
                 -skeleton_attr.shoulder.0,

mitmotion/hobania-update

voxygen/src/anim/src/biped_large/idle.rs
@@ -54,30 +54,37 @@ impl Animation for IdleAnimation {
                 * 0.125,
         );
 
+        // Controls for the beast breathing
+        let intensity = 0.04;
+        let lenght = 1.0;
+        let chop = 0.2;
+        let chop_freq = 40.0;
+
+        let breathing = intensity * (lenght * anim_time as f32).sin() + 0.05 * chop * (anim_time as f32 * chop_freq).sin() * (anim_time as f32 * lenght).cos();
+
         next.head.position = Vec3::new(
             0.0,
             skeleton_attr.head.0,
             skeleton_attr.head.1 + torso * 0.2,
         ) * 1.02;
-        next.head.orientation =
-            Quaternion::rotation_z(look.x * 0.6) * Quaternion::rotation_x(look.y * 0.6);
-        next.head.scale = Vec3::one() * 1.02;
+        next.head.orientation = Quaternion::rotation_z(look.x * 0.6) * Quaternion::rotation_x(look.y * 0.6 + breathing);
+        next.head.scale = Vec3::one() * 1.02 + breathing * 0.4;
 
         next.upper_torso.position = Vec3::new(
             0.0,
             skeleton_attr.upper_torso.0,
             skeleton_attr.upper_torso.1 + torso * 0.5,
         );
-        next.upper_torso.orientation = Quaternion::rotation_z(0.0);
-        next.upper_torso.scale = Vec3::one();
+        next.upper_torso.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(- breathing);
+        next.upper_torso.scale = Vec3::one() - breathing * 0.4;
 
         next.lower_torso.position = Vec3::new(
             0.0,
             skeleton_attr.lower_torso.0,
             skeleton_attr.lower_torso.1 + torso * 0.15,
         );
-        next.lower_torso.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
-        next.lower_torso.scale = Vec3::one() * 1.02;
+        next.lower_torso.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(breathing);
+        next.lower_torso.scale = Vec3::one() * 1.02 + breathing * 0.4;
         
         next.jaw.position = Vec3::new(
             0.0,
@@ -111,16 +118,16 @@ impl Animation for IdleAnimation {
             skeleton_attr.shoulder.1,
             skeleton_attr.shoulder.2,
         );
-        next.shoulder_l.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
-        next.shoulder_l.scale = Vec3::one();
+        next.shoulder_l.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(breathing);
+        next.shoulder_l.scale = Vec3::one() + breathing;
 
         next.shoulder_r.position = Vec3::new(
             skeleton_attr.shoulder.0,
             skeleton_attr.shoulder.1,
             skeleton_attr.shoulder.2,
         );
-        next.shoulder_r.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
-        next.shoulder_r.scale = Vec3::one();
+        next.shoulder_r.orientation = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(breathing);
+        next.shoulder_r.scale = Vec3::one() + breathing;
 
         next.hand_l.position = Vec3::new(
             -skeleton_attr.hand.0,

voxygen/src/anim/src/biped_large/wield.rs
@@ -27,6 +27,22 @@ impl Animation for WieldAnimation {
         let breathe = (anim_time as f32 + 1.5 * PI).sin();
         let test = (anim_time as f32 + 36.0 * PI).sin();
 
+        let slower = (anim_time as f32 * 1.0 + PI).sin();
+        let slow = (anim_time as f32 * 3.5 + PI).sin();
+        
+        let tailmove = Vec2::new(
+            ((global_time + anim_time) as f32 / 2.0)
+                .floor()
+                .mul(7331.0)
+                .sin()
+                * 0.25,
+            ((global_time + anim_time) as f32 / 2.0)
+                .floor()
+                .mul(1337.0)
+                .sin()
+                * 0.125,
+        );
+
         let look = Vec2::new(
             ((global_time + anim_time) as f32 / 8.0)
                 .floor()
@@ -126,12 +142,17 @@ impl Animation for WieldAnimation {
                 Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
             next.lower_torso.scale = Vec3::one() * 1.02;
 
-            next.jaw.position = Vec3::new(0.0, skeleton_attr.jaw.0, skeleton_attr.jaw.1 * 0.0);
-            next.jaw.orientation = Quaternion::rotation_z(0.0);
-            next.jaw.scale = Vec3::one();
-
+            next.jaw.position = Vec3::new(
+                0.0,
+                skeleton_attr.jaw.0 - slower * 0.12,
+                skeleton_attr.jaw.1 + slow * 0.2,
+            );
+            next.jaw.orientation = Quaternion::rotation_x(slow * 0.05);
+            next.jaw.scale = Vec3::one() * 1.02;
+    
             next.tail.position = Vec3::new(0.0, skeleton_attr.tail.0, skeleton_attr.tail.1);
-            next.tail.orientation = Quaternion::rotation_z(0.0);
+            next.tail.orientation =
+                Quaternion::rotation_z(0.0 + slow * 0.2 + tailmove.x) * Quaternion::rotation_x(0.0);
             next.tail.scale = Vec3::one();
 
             next.shoulder_l.position = Vec3::new(
@@ -213,6 +234,19 @@ impl Animation for WieldAnimation {
             next.lower_torso.orientation =
                 Quaternion::rotation_z(short * 0.15) * Quaternion::rotation_x(0.14);
             next.lower_torso.scale = Vec3::one() * 1.02;
+
+            next.jaw.position = Vec3::new(
+                0.0,
+                skeleton_attr.jaw.0 - slower * 0.12,
+                skeleton_attr.jaw.1 + slow * 0.2,
+            );
+            next.jaw.orientation = Quaternion::rotation_x(slow * 0.05);
+            next.jaw.scale = Vec3::one() * 1.02;
+    
+            next.tail.position = Vec3::new(0.0, skeleton_attr.tail.0, skeleton_attr.tail.1);
+            next.tail.orientation =
+                Quaternion::rotation_z(0.0 + slow * 0.2 + tailmove.x) * Quaternion::rotation_x(0.0);
+            next.tail.scale = Vec3::one();
 
             next.shoulder_l.position = Vec3::new(
                 -skeleton_attr.shoulder.0,