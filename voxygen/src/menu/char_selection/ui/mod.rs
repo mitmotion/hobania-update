42770c2f89bc50f8f5f7ee963f90a8f3687d3976
Imbris <imbrisf@gmail.com>
2020-12-04 00:38:26 -0500

    Render Ui

--- voxygen/src/menu/char_selection/ui/mod.rs
+++ voxygen/src/menu/char_selection/ui/mod.rs
@@ -1543,7 +1543,8 @@ impl CharSelectionUi {
     }
 
     // TODO: do we need globals?
-    pub fn render(&self, renderer: &mut Renderer) { self.ui.render(renderer); }
+    pub fn render(&self, renderer: &mut Renderer) { /* self.ui.render(renderer);*/
+    }
 }
 
 #[derive(Default)]
