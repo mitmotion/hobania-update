bcb62df8094a6dbf8525904a5062188a40c13dab
Ludvig Böklin <ludvig.boklin@protonmail.com>
2021-05-12 14:08:48 +0200

    Add lots of wind sources at the western border of map and make them tweakable

--- server/src/sys/windsim.rs
+++ server/src/sys/windsim.rs
@@ -7,6 +7,7 @@ use common::{
 use common_ecs::{Job, Origin, Phase, System};
 use specs::{Read, Write};
 use vek::*;
+use rand::Rng;
 
 /// This system updates the wind grid for the entire map
 #[derive(Default)]
@@ -20,18 +21,26 @@ impl<'a> System<'a> for Sys {
     const PHASE: Phase = Phase::Create;
 
     fn run(_job: &mut Job<Self>, (dt, mut windsim): Self::SystemData) {
-        let wind_sources: Vec<(Pos, Vel)> = vec![(
-            Pos(Vec3 {
-                x: 9999.0,
-                y: 9599.0,
-                z: 200.0,
-            }),
-            Vel(Vec3 {
-                x: 100.0,
-                y: 100.0,
-                z: 500.0,
-            }),
-        )];
+        let mut rng = rand::thread_rng();
+        let wind_sources: Vec<(Pos, Vel)> = (0..1000)
+            .map(|y| {
+                let r1 = rng.gen_range(0.0..std::f32::consts::PI / 2.0);
+                let r2 = rng.gen_range(0.0..std::f32::consts::PI / 2.0);
+                let r = (r1 + r2) / 2.0;
+                (
+                    Pos(Vec3 {
+                        x: inline_tweak::tweak!(5000.0),
+                        y: y as f32 * 32.0,
+                        z: inline_tweak::tweak!(200.0),
+                    }),
+                    Vel(Vec3 {
+                        x: inline_tweak::tweak!(30.0) * r.sin(),
+                        y: inline_tweak::tweak!(30.0) * r.cos(),
+                        z: inline_tweak::tweak!(30.0) * rng.gen_range(-0.25..0.25),
+                    }),
+                )
+            })
+            .collect::<Vec<_>>();
         windsim.tick(wind_sources, &dt);
     }
 }
