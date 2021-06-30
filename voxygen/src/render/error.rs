164bb4f87fc1b2a384c017c81f1c5dfab452ac1c
João Capucho <jcapucho7@gmail.com>
2021-06-30 16:10:24 +0100

    DOESN'T WORK
    I made this commit because I've accidentally broke this when rebasing
    and deleted the veloren folder and don't want to write this for the 4th
    time.
    
    Please ignore this.

--- voxygen/src/render/error.rs
+++ voxygen/src/render/error.rs
@@ -6,8 +6,10 @@ pub enum RenderError {
     SwapChainError(wgpu::SwapChainError),
     CustomError(String),
     CouldNotFindAdapter,
-    ErrorInitializingCompiler,
-    ShaderError(String, shaderc::Error),
+    GlslIncludeError(String, glsl_include::Error),
+    ParserError(String, naga::front::glsl::ParseError),
+    ValidationError(String, naga::valid::ValidationError),
+    SpirvError(String, naga::back::spv::Error),
 }
 
 use std::fmt;
@@ -25,10 +27,24 @@ impl fmt::Debug for RenderError {
                 .finish(),
             Self::CustomError(err) => f.debug_tuple("CustomError").field(err).finish(),
             Self::CouldNotFindAdapter => f.debug_tuple("CouldNotFindAdapter").finish(),
-            Self::ErrorInitializingCompiler => f.debug_tuple("ErrorInitializingCompiler").finish(),
-            Self::ShaderError(shader_name, err) => write!(
+            Self::GlslIncludeError(shader_name, err) => write!(
                 f,
-                "\"{}\" shader failed to compile due to the following error: {}",
+                "\"{}\" shader contains invalid include directives: {}",
+                shader_name, err
+            ),
+            Self::ParserError(shader_name, err) => write!(
+                f,
+                "\"{}\" shader failed to parse due to the following error: {}",
+                shader_name, err
+            ),
+            Self::ValidationError(shader_name, err) => write!(
+                f,
+                "\"{}\" shader failed to validate due to the following error: {}",
+                shader_name, err
+            ),
+            Self::SpirvError(shader_name, err) => write!(
+                f,
+                "\"{}\" shader failed to emit due to the following error: {}",
                 shader_name, err
             ),
         }
@@ -47,8 +63,24 @@ impl From<wgpu::SwapChainError> for RenderError {
     fn from(err: wgpu::SwapChainError) -> Self { Self::SwapChainError(err) }
 }
 
-impl From<(&str, shaderc::Error)> for RenderError {
-    fn from((shader_name, err): (&str, shaderc::Error)) -> Self {
-        Self::ShaderError(shader_name.into(), err)
+impl From<(&str, glsl_include::Error)> for RenderError {
+    fn from((shader_name, err): (&str, glsl_include::Error)) -> Self {
+        Self::GlslIncludeError(shader_name.into(), err)
+    }
+}
+
+impl From<(&str, naga::front::glsl::ParseError)> for RenderError {
+    fn from((shader_name, err): (&str, naga::front::glsl::ParseError)) -> Self {
+        Self::ParserError(shader_name.into(), err)
+    }
+}
+impl From<(&str, naga::valid::ValidationError)> for RenderError {
+    fn from((shader_name, err): (&str, naga::valid::ValidationError)) -> Self {
+        Self::ValidationError(shader_name.into(), err)
+    }
+}
+impl From<(&str, naga::back::spv::Error)> for RenderError {
+    fn from((shader_name, err): (&str, naga::back::spv::Error)) -> Self {
+        Self::SpirvError(shader_name.into(), err)
     }
 }
